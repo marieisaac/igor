8e6a67a01ed5346329056909794e2e60bc94f149
marieisaac <38866609+marieisaac@users.noreply.github.com>
2023-09-30 16:52:13 -0400

    Update binarywave.py

--- igor/binarywave.py
+++ igor/binarywave.py
@@ -107,11 +107,11 @@ class NullStaticStringField (StaticStringField):
 # From IgorMath.h
 TYPE_TABLE = {        # (key: integer flag, value: numpy dtype)
     0:None,           # Text wave, not handled in ReadWave.c
-    1:_numpy.complex, # NT_CMPLX, makes number complex.
+    1:complex, # NT_CMPLX, makes number complex.
     2:_numpy.float32, # NT_FP32, 32 bit fp numbers.
-    3:_numpy.complex64,
+    3:complex64,
     4:_numpy.float64, # NT_FP64, 64 bit fp numbers.
-    5:_numpy.complex128,
+    5:complex128,
     8:_numpy.int8,    # NT_I8, 8 bit signed integer. Requires Igor Pro
                       # 2.0 or later.
     9:complexInt8,
